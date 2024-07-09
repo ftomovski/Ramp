6690f5d1e8d5693aa384cd51855808d2acbd9d5d
Filip T <88133428+ftomovski@users.noreply.github.com>
2024-07-09 23:27:27 +0000

    finals

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -22,3 +22,29 @@ export const transactionToRow = (
     ),
   }
 }
+import { Transaction } from "./types"
+import { InputCheckbox } from "../components/InputCheckbox"
+
+export const transactionToRow = (
+  transaction: Transaction,
+  toggleTransactionApproval: (id: string, approved: boolean) => void
+) => {
+  const approveComponent = (
+    <InputCheckbox
+      key={transaction.id}
+      id={transaction.id}
+      checked={transaction.approved}
+      onChange={(checked) => toggleTransactionApproval(transaction.id, checked)}
+    />
+  )
+
+  return {
+    id: transaction.id,
+    merchant: transaction.merchant,
+    employee: transaction.employee,
+    amount: transaction.amount,
+    status: transaction.status,
+    receipt: transaction.receipt,
+    approve: approveComponent,
+  }
+}
